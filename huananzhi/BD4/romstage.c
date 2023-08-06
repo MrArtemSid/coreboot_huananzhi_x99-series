91ea679fd73e806a667265d41df6381bdb24645d
MrArtemSid <artem.sid.key@gmail.com>
2023-08-06 17:23:42 +0400

    huananzhi: BD4: Add BD4 changes

--- huananzhi/BD4/romstage.c
+++ huananzhi/BD4/romstage.c
@@ -26,20 +26,20 @@ void mb_get_spd_map(struct spd_info *spdi)
 const struct usb2_port_config mainboard_usb2_ports[MAX_USB2_PORTS] = {
 	/* FIXME: Length and Location are computed from IOBP values, may be inaccurate */
 	/* Length, Enable, OCn#, Location */
-	{ 0x0110, 1, 0, USB_PORT_BACK_PANEL },
-	{ 0x0140, 1, 0, USB_PORT_BACK_PANEL },
-	{ 0x0040, 1, 1, USB_PORT_FLEX },
-	{ 0x0040, 1, 1, USB_PORT_FLEX },
-	{ 0x0040, 1, 2, USB_PORT_FLEX },
-	{ 0x0040, 1, 2, USB_PORT_FLEX },
+	{ 0x0110, 1, USB_OC_PIN_SKIP, USB_PORT_BACK_PANEL },
+	{ 0x0140, 1, USB_OC_PIN_SKIP, USB_PORT_BACK_PANEL },
+	{ 0x0040, 1, USB_OC_PIN_SKIP, USB_PORT_FLEX },
+	{ 0x0040, 1, USB_OC_PIN_SKIP, USB_PORT_FLEX },
+	{ 0x0040, 1, USB_OC_PIN_SKIP, USB_PORT_FLEX },
+	{ 0x0040, 1, USB_OC_PIN_SKIP, USB_PORT_FLEX },
 	{ 0x0040, 1, 3, USB_PORT_FLEX },
 	{ 0x0040, 1, 3, USB_PORT_FLEX },
-	{ 0x0140, 1, 4, USB_PORT_BACK_PANEL },
-	{ 0x0140, 1, 4, USB_PORT_BACK_PANEL },
-	{ 0x0140, 1, 5, USB_PORT_BACK_PANEL },
-	{ 0x0140, 1, 5, USB_PORT_BACK_PANEL },
-	{ 0x0040, 1, 6, USB_PORT_FLEX },
-	{ 0x0040, 1, 6, USB_PORT_FLEX },
+	{ 0x0140, 1, USB_OC_PIN_SKIP, USB_PORT_BACK_PANEL },
+	{ 0x0140, 1, USB_OC_PIN_SKIP, USB_PORT_BACK_PANEL },
+	{ 0x0140, 1, USB_OC_PIN_SKIP, USB_PORT_BACK_PANEL },
+	{ 0x0140, 1, USB_OC_PIN_SKIP, USB_PORT_BACK_PANEL },
+	{ 0x0040, 1, USB_OC_PIN_SKIP, USB_PORT_FLEX },
+	{ 0x0040, 1, USB_OC_PIN_SKIP, USB_PORT_FLEX },
 };
 
 const struct usb3_port_config mainboard_usb3_ports[MAX_USB3_PORTS] = {
